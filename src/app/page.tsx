415813b2bbd6225f18319ed92ad140b027ed2c6c
Elena Nikiforova <vizualnoe@gmail.com>
2023-08-01 16:28:56 +0200

    feat: add recipe widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import { useState } from 'react';
 import { MySwiper, MySwiper2 } from '@/components/Swiper/Swiper'
 import { RecipeWidget } from '@/components/RecipeWidget/RecipeWidget'
+import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function Home() {
+
+  const [RecipeWidgetOpen, setRecipeWidgetOpen] = useState(true);
+
   return (
     <main className={styles.main}>
-      <RecipeWidget />
+      {
+        RecipeWidgetOpen && <RecipeWidget close={() => setRecipeWidgetOpen(false)}/>
+      }
       <h2>Мои рецепты</h2>
       <MySwiper />
       <h2>Мои лайфхаки</h2>
--- src/components/RecipeWidget/RecipeWidget.tsx
+++ src/components/RecipeWidget/RecipeWidget.tsx
@@ -1,9 +1,37 @@
-export const RecipeWidget = () => {
+'use client';
+
+import image1 from '@/assets/images/mussels.jpg';
+import Image from 'next/image';
+
+import { FC } from "react";
+
+import CloseIcon from '@/assets/icons/close-icon.svg';
+
+import styles from './RecipeWidget.module.css';
+
+interface RecipeWidgetProps {
+  close: ()=> void
+}
+
+export const RecipeWidget: FC<RecipeWidgetProps> = ({ close }) => {
 
   return (
     <section>
-      <article>
-        <h2>RecipeWidget</h2>
+      <article className={styles.article}>
+        <Image 
+          src={image1} 
+          alt="mussels" 
+          className={styles.image}
+        />
+        <div className={styles.info}>
+          <div className={styles.clock}></div>
+          <p className={styles.recipeTitle}>Венские вафли с малиной и кленовым сиропом</p>
+        </div>
+
+        <button type="button" onClick={()=> close()} className={styles.button}>
+          <Image src={CloseIcon} alt="close button" />
+        </button>
+
       </article>
     </section>
   )
